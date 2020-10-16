69f9f04cc2eef4c68303258d63b1d0ae2fe91335
Adrian Mateos <adrian@adrian>
2020-10-16 11:46:43 +0200

    [ADD] Añadido ejercicio de fecha con maven

---
+++ src/main/java/adriControl/Controlador.java
@@ -0,0 +1,52 @@
+package adriControl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controlador
+ */
+@WebServlet("/Controlador")
+public class Controlador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controlador() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		Date date = new Date();
+		
+		out.print("<html><body>");
+		out.print("<h1>La fecha es: " + date);
+		out.print("</h1></body></html>");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<form action="http://localhost:8080/barras/Controlador" method="post">
+    <label for="hora">Si quieres saber la fecha pulsa el botón: </label> <input type="submit" value="púlsame">
+</form>
+</body>
+</html>
